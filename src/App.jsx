588dc90d6246aec30bdf9a1f8b689b293e985911
Mauriciohnf45 <mh173509@gmail.com>
2024-10-19 08:57:54 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import VerificarNota from './components/VerificarNota';
+
+function App() {
+  return (
+    <div className="App">
+      <VerificarNota nota={85} />
+    </div>
+  );
+}
+
+export default App;
